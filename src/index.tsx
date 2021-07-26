7179f30eb8029749593e219cedd0848a8c550382
jhosep98 <jhosep149@hotmail.com>
2021-07-26 19:41:27 -0300

    Setup apollo client to react

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/client";
+import "./index.css";
+
+import App from "./App";
+import { client } from "./api/client";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
